0b27f3988154c70d8953844f11e935b8af8281d7
nypzlst <91936573+nypzlst@users.noreply.github.com>
2022-04-18 16:21:36 +0300

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -1,43 +1,15 @@
+var ProxyUrl ='https://cors-anywhere.herokuapp.com/'
+    TargetUrl = 'https://alarmmap.online/assets/alerts.json'
 
-//let requestUrl = 'https://alarmmap.online/assets/alerts.json'
-/*
-//console.log(requestUrl);
-fetch('https://alarmmap.online/assets/alerts.json',{
-    mode:'no-cors'
-})
-    .then(res => res.json())
-    
-
-/*
-*/
-/*fetch('https://alarmmap.online/assets/alerts.json',{ 
-    mode: 'no-cors' // 'cors' by default
-}).then(response => response.json) {
-    
-}).then(function(data){
-  
-});
-*//*
-fetch('https://alarmmap.online/assets/alerts.json',{ 
-    mode: 'no-cors' // 'cors' by default
-}).then(res => console.log(Response.json))
-  .then(data=> console.log(data))
-
-
-let data;
-
-fetch('https://alarmmap.online/assets/alerts.json',{mode:'no-cors'})
-.then(response => {
-   data = response.json();
-});
-
-console.log(data);
-
-*/
-async function getResponse(){
-    let response = await fetch('https://alarmmap.online/assets/alerts.json',{mode:'no-cors'})
-    let content  = await response.json()
-    console.log(content)
-}
-getResponse()
-
+fetch(ProxyUrl + TargetUrl)
+    .then(Blob => Blob.json())
+    .then(data=>{
+        console.table(data);
+        document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
+        return data;
+    //    console.log(JSON.stringify(data))
+    })   
+    .catch(e => {
+        console.log(e);
+        return e;
+      }); 
