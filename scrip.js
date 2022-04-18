0a51ef7437ef5b5d4bbe88a25d89cb18c9a39291
nypzlst <91936573+nypzlst@users.noreply.github.com>
2022-04-18 15:14:07 +0300

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -1,5 +1,43 @@
-fetch('https://emapa.fra1.cdn.digitaloceanspaces.com/statuses.json',{ 
+
+//let requestUrl = 'https://alarmmap.online/assets/alerts.json'
+/*
+//console.log(requestUrl);
+fetch('https://alarmmap.online/assets/alerts.json',{
+    mode:'no-cors'
+})
+    .then(res => res.json())
+    
+
+/*
+*/
+/*fetch('https://alarmmap.online/assets/alerts.json',{ 
     mode: 'no-cors' // 'cors' by default
-}).then(function(response) {
-    // Do something with response
+}).then(response => response.json) {
+    
+}).then(function(data){
+  
 });
+*//*
+fetch('https://alarmmap.online/assets/alerts.json',{ 
+    mode: 'no-cors' // 'cors' by default
+}).then(res => console.log(Response.json))
+  .then(data=> console.log(data))
+
+
+let data;
+
+fetch('https://alarmmap.online/assets/alerts.json',{mode:'no-cors'})
+.then(response => {
+   data = response.json();
+});
+
+console.log(data);
+
+*/
+async function getResponse(){
+    let response = await fetch('https://alarmmap.online/assets/alerts.json',{mode:'no-cors'})
+    let content  = await response.json()
+    console.log(content)
+}
+getResponse()
+
